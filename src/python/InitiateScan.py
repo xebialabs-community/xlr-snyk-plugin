2c67e5a7cd63eed7d17c185d41c0b88513dbe5ea
David Christensen <shorawitz@gmail.com>
2020-09-14 12:09:30 -0700

    Update InitiateScan.py with some initial testing code

--- src/python/InitiateScan.py
+++ src/python/InitiateScan.py
@@ -1,1 +1,38 @@
+import requests
+  
+API_KEY = '3e1a4b98-750c-431a-bbd6-4b600409dd60'
+ORG_KEY = '2f49febc-d075-4ebe-b9e8-daa34eae6b07'
+base_url = 'https://snyk.io/api/v1/'
+#endpoint = '/org/shorawitz-x8m/projects'
+endpoint = '/test/GitHub/saltstack/salt'
+
+##
+# enpoint: orgs, org/:id/projects, test/:packageManager/:packageName/:packageVersion
+def get_endpoint(base_url, endpoint, API_KEY):
+    print("Access Snyk endpoint...")
+
+    headers = {'Authorization': 'token ' + API_KEY}
+    resp = requests.get(base_url + endpoint, headers=headers)
+
+    if resp.ok:
+        #print(resp.json())
+        return resp
+    else:
+        # Something went wrong
+        print("HTTP %i - %s, Message %s" % (resp.status_code, resp.reason, resp.text))
+        return False
+
+
+def main():
+    resp = get_endpoint(base_url, endpoint, API_KEY)
+
+    if resp and type(resp.json()) == dict:
+        print("We got a dict return")
+        print(resp.json())
+    else:
+        print("Not what we expected!")
+
+
+## Run it
+
 print "Scan completed"
